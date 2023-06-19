91703e0b67add29800a2c93ee8479b297d9cbb68
eduardoecheverria <1035514-at-512567350393>
2023-06-19 07:47:47 -0600

    add react to app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
+import React from 'react';
+import * as ReactDOM from 'react-dom';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const rootDomElement = document.getElementById('root');
+const root = ReactDOM.findDOMNode(rootDomElement);
+const appCode = React.createElement('h1',null,'hola mundo')
 const analytics = getAnalytics(app);
--- src/reactDesdeCero/src/index.js
+++ src/reactDesdeCero/src/index.js
@@ -1,5 +1,8 @@
-import './styles.css';
+import './styles.css'
+import React from "react"
+import * as ReactDom from "react-dom/client"
+import App from './App.js'
 
-const container = document.getElementById("app").innerHTML;
-
+const container = document.getElementById("app");
 const root = createRoot(container);
+root.render(<App />)
